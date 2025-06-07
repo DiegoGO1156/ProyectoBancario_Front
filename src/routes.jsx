d445e8679076ee5d98f741253914caf6bbb9e8f2
josecipriano <ciprianoaceytunoj@gmail.com>
2025-06-07 01:32:34 -0600

    homepage

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,8 +1,10 @@
 import { Auth } from '../src/pages/Auth';
+import  HomePage  from '../src/pages/HomePage';
 
 
 const routes = [
     {path: '/auth', element: <Auth />},
+    {path: '/home', element: <HomePage />},
 ]
 
 export default routes
